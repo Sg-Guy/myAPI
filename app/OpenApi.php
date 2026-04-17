eff21c4e0ec76ea82679633693c90712466ce513
Guillaume <gsagbo541@gmail.com>
2026-04-17 15:16:33 +0100

    swagger correction

--- app/OpenApi.php
+++ app/OpenApi.php
@@ -7,21 +7,21 @@ use OpenApi\Attributes as OA;
 //Info App
 #[OA\Info(
     version: "1.0.0",
-    title: "Shop API",
-    description: "Documentation officielle de l'API Shop"
+    title: "MyAPI",
+    description: "Documentation officielle de MyAPI"
 )]
 
 
 //Infos server
 #[OA\Server(
-    url: "http://127.0.0.1:8000",
+    url:  "https://myapi-o37g.onrender.com",// "http://127.0.0.1:8000",
     description: "Serveur local"
 )]
 
 //Pour l'authentification
 #[OA\SecurityScheme(
     securityScheme: "sanctum",
-    type: "http",
+    type: "https",
     scheme: "bearer",
     bearerFormat: "JWT"
 )]
